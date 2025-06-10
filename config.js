c4f5aa4b784101662fcac1f2f9d19b9aa74056ca
marcelwow <marcelek.waw@gmail.com>
2025-06-11 01:02:30 +0200

    ok

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const PORT = 3000;
 
-const DB_USER = "marcelekwaw";
-const DB_PASS = "marcel1234";
+const DB_USER = "";
+const DB_PASS = "";
 
 
 const MONGODB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.86jxk9y.mongodb.net/tracklearn?retryWrites=true&w=majority`;
